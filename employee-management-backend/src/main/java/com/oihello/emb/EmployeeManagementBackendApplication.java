c2c993efc932861e44163278ef6f4491a8c9f9b8
samjubayer <samjubayer@192.168.0.16>
2021-05-19 23:05:11 +0900

    c-02: added Component annotation

--- employee-management-backend/src/main/java/com/oihello/emb/EmployeeManagementBackendApplication.java
+++ employee-management-backend/src/main/java/com/oihello/emb/EmployeeManagementBackendApplication.java
@@ -2,8 +2,10 @@ package com.oihello.emb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@Component
 public class EmployeeManagementBackendApplication {
 
 	public static void main(String[] args) {
